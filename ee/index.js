ad8299490ddf12094fed47076c21438eb0838713
Charly <63873592+Charleslogan99@users.noreply.github.com>
2022-12-04 20:26:01 +0100

    Add files via upload

---
+++ ee/index.js
@@ -0,0 +1,9 @@
+let user=prompt("Enter name here")
+if (user=="do_stuff") {alert("Ok, i will do stuff...")}
+else {alert("You did not tell me something i know!")}; 
+
+let item = {
+    toaster: "10000",
+    blender: "25000",
+    microwave: "60000"
+}
